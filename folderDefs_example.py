4859754277130b32d038c939abf6534821d6456e
Gael Reinaudi <gael.reinaudi@gmail.com>
2018-02-16 15:42:39 +0100

    Updated version (reverted from commit d961af8cdc601a854418282b95fb8615c5d7446d)

---
+++ folderDefs_example.py
@@ -0,0 +1,9 @@
+
+LogDirMain  = './logs'
+
+trainingDataDir  = './data/'
+
+trainingDataDirRaw  = '/media/aquaplanet_training_data/'
+raw_file_base = 'AndKua_aqua_SPCAM3.0.cam2.h1.0000-01-01-00000.nc'
+
+trainingDataDirTFRecords  = trainingDataDir+'tfRecords/'
